408682b9a60b9ca046b44eff5306abe821280346
Vitor HP Bittencourt <vitorhp2@gmail.com>
2017-03-24 00:39:08 -0300

    More tests

--- lib/planet.ex
+++ lib/planet.ex
@@ -1,7 +1,28 @@
 defmodule Cosmic.Planet do
-  defstruct user_id: nil, id: nil, ships: %{}
+  defstruct color: nil, id: nil, ships: %{}
 
   def add_ships(planet, {color, amount}) do
-    %Cosmic.Planet{ planet | ships: Map.update(planet.ships, color, amount, fn(initial) -> initial + amount end) }
+    %Cosmic.Planet{ planet |
+      ships: Map.update(planet.ships,
+                        color,
+                        amount,
+                        fn(initial) -> initial + amount end) }
+  end
+
+  def remove_ships(planet, {color, amount}) do
+    %Cosmic.Planet{ planet |
+      ships: Map.update(planet.ships, color, amount, fn
+                          initial when initial - amount >= 0 -> initial - amount
+                          initial -> initial
+                        end)
+    }
+  end
+
+  def has_control?(planet, color) do
+    planet.color == color && Map.get(planet.ships, color, 0) > 0
+  end
+
+  def has_colony?(planet, color) do
+    planet.color != color && Map.get(planet.ships, color, 0) > 0
   end
 end
--- test/lib/planet_test.exs
+++ test/lib/planet_test.exs
@@ -3,7 +3,7 @@ defmodule Cosmic.PlanetTest do
   alias Cosmic.Planet
 
   test "planet has right attributes" do
-    assert 1 == struct(Planet, %{user_id: 1}).user_id
+    assert 'blue' == struct(Planet, %{color: 'blue'}).color
     assert 2 == struct(Planet, %{id: 2}).id
     assert %{} == struct(Planet, %{}).ships
   end
@@ -19,6 +19,48 @@ defmodule Cosmic.PlanetTest do
 
     test "adding ships to another color" do
       assert %Planet{ ships: %{ "blue" => 1, "red" => 2 } } == Planet.add_ships(%Planet{ships: %{ "blue" => 1 }}, { "red", 2 })
+    end
+  end
+
+  describe "#remove_ships" do
+    test "from existing color" do
+      assert %Planet{ ships: %{ "blue" => 0 } } == Planet.remove_ships(%Planet{ ships: %{ "blue" => 2 }}, { "blue", 2 })
+    end
+
+    test "removing more ships than there are ships to remove" do
+      assert %Planet{ ships: %{ "blue" => 2 } } == Planet.remove_ships(%Planet{ ships: %{ "blue" => 2 }}, { "blue", 3 })
+    end
+  end
+
+  describe "#has_control?" do
+    test "of own planets where it has ships" do
+      assert true == Planet.has_control?(%Planet{ color: "blue", ships: %{ "blue" => 2 }}, "blue")
+    end
+
+    test "of own planets where it has no ships" do
+      assert false == Planet.has_control?(%Planet{ color: "blue", ships: %{ "blue" => 0 }}, "blue")
+    end
+
+    test "of other planets where it has ships" do
+      assert false == Planet.has_control?(%Planet{ color: "red", ships: %{ "blue" => 2 }}, "blue")
+    end
+
+    test "of other planets where it has no ships" do
+      assert false == Planet.has_control?(%Planet{ color: "red", ships: %{ "red" => 2 }}, "blue")
+    end
+  end
+
+  describe "#has_colony?" do
+    test "in own planet" do
+      assert false == Planet.has_colony?(%Planet{ color: "blue", ships: %{ "blue" => 2 }}, "blue")
+    end
+
+    test "in other planets where it has ships" do
+      assert true == Planet.has_colony?(%Planet{ color: "red", ships: %{ "blue" => 2 }}, "blue")
+    end
+
+    test "in other planets where it has no ships" do
+      assert false == Planet.has_colony?(%Planet{ color: "red", ships: %{ "red" => 2 }}, "blue")
     end
   end
 end
