47cdf0fa939a404a56bc4fe5661bef415d5e8454
Vitor HP Bittencourt <vitorhp2@gmail.com>
2017-03-31 17:51:20 -0300

    Using Streams to avoid reiterating array

--- lib/game.ex
+++ lib/game.ex
@@ -24,8 +24,8 @@ defmodule Cosmic.Game do
   def score(game) do
     game
     |> Map.get(:players)
-    |> Enum.flat_map(&(Map.get(&1, :planets)))
-    |> Enum.flat_map(&(Planet.colony_colors(&1)))
+    |> Stream.flat_map(&Map.get(&1, :planets))
+    |> Stream.flat_map(&Planet.colony_colors(&1))
     |> Enum.reduce(%{}, fn(color, acc) -> Map.update(acc, color, 1, &(&1 + 1)) end)
   end
 
